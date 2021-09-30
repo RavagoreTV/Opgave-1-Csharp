33409bfacbfdc9c0d3b9ba5fe6437d35fc0fbe4e
Victor Dahl Saltoft <vict2729@edu.easj.dk>
2021-09-30 12:31:43 +0200

    Add project files.

---
+++ Opgave 1 Csharp/FootballPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Opgave_1_Csharp
+{
+    public class FootballPlayer
+    {
+        public FootballPlayer()
+        {
+            
+        }
+
+        private string _name;
+        private int _price;
+        private int _shirtNumber;
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length <= 4) throw new ArgumentException();
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException();
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value <= 1 || value >= 100) throw new ArgumentOutOfRangeException();
+                {
+                    _shirtNumber = value;
+                }
+            }
+        }
+    }
+}
---
+++ Opgave 1 CsharpTests/FootballPlayerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave_1_Csharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_1_Csharp.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void NameTest()
+        {
+            FootballPlayer footballPlayer = new FootballPlayer();
+            footballPlayer.Name = "Victor";
+
+            Assert.AreEqual("Victor", footballPlayer.Name);
+
+            Assert.ThrowsException<ArgumentException>(
+                () => footballPlayer.Name = "V");
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            FootballPlayer footballPlayer = new FootballPlayer();
+            footballPlayer.Price = 1000000;
+
+            Assert.AreEqual(1000000, footballPlayer.Price);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => footballPlayer.Price = -650);
+        }
+
+        [TestMethod()]
+        public void ShirtNumberTest()
+        {
+            FootballPlayer footballPlayer = new FootballPlayer();
+            footballPlayer.ShirtNumber = 50;
+
+            Assert.AreEqual(50, footballPlayer.ShirtNumber);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => footballPlayer.ShirtNumber = 0);
+        }
+    }
+}
